a39c0eb71549b17475042e347470beb65e67b60d
EmmaKibore <emmakibore@gmail.com>
2019-04-27 22:57:32 +0300

    made structural changes to manage.py

--- app/email.py
+++ app/email.py
@@ -2,7 +2,6 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-<<<<<<< HEAD
 def mail_message(subject,template,to,**kwargs):
    sender_email = 'emmaKibore@gmail.com'
 
@@ -10,12 +9,3 @@ def mail_message(subject,template,to,**kwargs):
    email.body= render_template(template + ".txt",**kwargs)
    email.html = render_template(template + ".html",**kwargs)
    mail.send(email)
-=======
-def mail_message(subject,template,to, **kwargs):
-    sender_email = 'emmaKibore@gmail.com'
-
-    email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt", **kwargs)
-    email.html = render_template(template + ".html", **kwargs)
-    mail.send(email)
->>>>>>> 14aae2dcc0c6ce0324a855cec00bb71830ae2709
--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from app import create_app ,db
 from flask_script import Manager,Server
 from app.models import User, Comment, Post
@@ -10,20 +9,6 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
-=======
-from app import create_app,db
-from app.models import User,Comment,Pitch
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager,Server,Shell
-
-app = create_app('development')
-
-manager = Manager(app)
-migrate = Migrate(app,db)
-
-manager.add_command('server',Server)
-manager.add_command('db',MigrateCommand)
->>>>>>> 14aae2dcc0c6ce0324a855cec00bb71830ae2709
 
 @manager.command
 def test():
@@ -32,7 +17,6 @@ def test():
    tests = unittest.TestLoader().discover('tests')
    unittest.TextTestRunner(verbosity=2).run(tests)
 
-<<<<<<< HEAD
 @manager.shell
 def make_shell_context():
     return dict(app = app, db = db, User = User, Comment = Comment, Post = Post )
@@ -42,14 +26,3 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
    manager.run()
-=======
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db, User = User,Pitch = Pitch, Comment = Comment )
-
-
-
-if __name__ == '__main__':
-    manager.run()
->>>>>>> 14aae2dcc0c6ce0324a855cec00bb71830ae2709
